f2b13a50f21f2f3b52dd8c4d3c01db291c51fbeb
AmirHossein <khanzadeh78ah@gmail.com>
2023-09-25 02:12:29 +0330

    :sparkles: Added admin view

--- apps/people/admin.py
+++ apps/people/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import People1399, People1400, Family1399, Family1400
+from .models import (
+    People1399,
+    People1400,
+    Family1399,
+    Family1400,
+    SamePeople,
+    SameFamilies,
+)
 
 
 class PeopleAdmin(admin.ModelAdmin):
@@ -10,7 +17,18 @@ class FamilyAdmin(admin.ModelAdmin):
     list_display = ["ParentID", "MembersID"]
 
 
+class SamePeopleAdmin(admin.ModelAdmin):
+    list_display = ["id_1399", "id_1400"]
+
+
+class SameFamiliesAdmin(admin.ModelAdmin):
+    list_display = ["parentid_1399", "parentid_1400"]
+
+
 admin.site.register(People1399, PeopleAdmin)
 admin.site.register(People1400, PeopleAdmin)
 admin.site.register(Family1399, FamilyAdmin)
 admin.site.register(Family1400, FamilyAdmin)
+
+admin.site.register(SamePeople, SamePeopleAdmin)
+admin.site.register(SameFamilies, SameFamiliesAdmin)
